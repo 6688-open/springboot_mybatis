42fa3b1b28e89fd6054c53819a0844d9e4c5a395
wj <18351867657@163.com>
2021-11-05 10:26:18 +0800

    初始化

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * http://localhost:8080/testBoot/getUser?id=1
+ */
 @MapperScan("com.example.mapper") //扫描的mapper
 @SpringBootApplication
 public class DemoApplication {
